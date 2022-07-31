d6f25e8b4b6c5176f64f2b39d35a5a760dea70e5
Joel Shapiro <joeladamshapiro@gmail.com>
2022-07-31 12:54:15 -0400

    Makes game functional with constant player input

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ switch (randomNumber) {
 }
 }
 console.log(getComputerChoice());
+
+const playerSelection = "rock";
+playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'rock') {
+        alert("Tie Game. Play again.");
+    } else if (playerSelection === 'rock' && computerSelection == 'paper') {
+        alert("You lose! Paper beats Rock.");
+    } else if (playerSelection === 'rock' && computerSelection == 'scissors') {
+        alert("You win! Rock beats scissors.");
+    }
+    }
+
+console.log(playRound(playerSelection, computerSelection));
