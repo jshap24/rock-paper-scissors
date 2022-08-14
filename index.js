1a0360271195e71e2d4d778869729731d14e10d7
Joel Shapiro <joeladamshapiro@gmail.com>
2022-08-14 11:22:59 -0400

    Adds UI to Rock Paper Scissors Game

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 
+let user = ["Player", "Computer"];
 let playerScore = 0
 let computerScore = 0
+let gameIsOver = false;
 
+const rockBtn = document.getElementById("rockBtn");
+rockBtn.addEventListener("click", function(e) {
+    const playerSelection = 'rock';
+    console.log(playerSelection);
+    game('rock');
+})
 
-function game() {
+const paperBtn = document.getElementById("paperBtn");
+paperBtn.addEventListener("click", function(e) {
+    const playerSelection = 'paper';
+    console.log(playerSelection);
+    game('paper');
+})
+
+const scissorsBtn = document.getElementById("scissorsBtn");
+scissorsBtn.addEventListener("click", function(e) {
+    const playerSelection = 'scissors';
+    console.log(playerSelection);
+    game('scissors');
+})
+
+function game(playerSelection) {
 
    
 function getComputerChoice() {
@@ -27,28 +49,27 @@ function getComputerChoice() {
     }
 
 
-let playerInput = prompt("Rock Paper Scissors, shoot!");
 
-
+const computerSelection = getComputerChoice();
 
 function playRound() {
 
-
-    const playerSelection = playerInput.toLowerCase();
-    const computerSelection = getComputerChoice();
-    
     if (playerSelection == 'rock' && computerSelection == 'rock') {
         return "Tie Game. Play again.";
     } else if (playerSelection === 'rock' && computerSelection == 'paper') {
         computerScore = computerScore + 1;
+        endGame();
         return "You lose! Paper beats Rock.";
     } else if (playerSelection === 'rock' && computerSelection == 'scissors') {
         playerScore++;
+        endGame();
         return ("You win! Rock beats scissors.");
     } else if (playerSelection === 'scissors' && computerSelection == 'scissors') {
+        endGame();
         return ("Tie Game. Play again.");
     } else if (playerSelection === 'scissors' && computerSelection == 'rock') {
         computerScore++;
+        endGame();
         return ("You lose! Rock beats scissors.");
     } else if (playerSelection === 'scissors' && computerSelection == 'paper') {
         playerScore++;
@@ -56,26 +77,71 @@ function playRound() {
         
     } else if (playerSelection === 'paper' && computerSelection == 'scissors') {
         computerScore++;
+        endGame();
         return ("You lose! Scissors beats paper.");
-        
     } else if (playerSelection === 'paper' && computerSelection == 'rock') {
         playerScore++;
+        endGame();
         return ("You win! Paper beats rock.");
         
     } else if (playerSelection === 'paper' && computerSelection == 'paper') {
         return ("Tie Game. Play again.");
     } else {
         return ("Not a valid entry. Try again");
+    }   
+}
+
+const resultSelection = document.getElementById("result-selection");
+resultSelection.innerHTML = playRound();
+const yourScoreBoard = document.getElementById("yourScore").innerHTML = playerScore;
+const computerScoreBoard = document.getElementById("computerScore").innerHTML = computerScore;
+}
+ 
+function addSelectionResult() {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div);
+}
+
+let winnerName = document.getElementById("winnerName")
+
+let isGameOver = (score) => {
+    if (playerScore === 5 || computerScore === 5) {
+        return true;
     }
-
+    return false;
 }
 
-console.log(playRound());
-console.log(playerScore + "vs." + computerScore);
+function endGame() {
+    console.log(isGameOver());
+    if ( isGameOver() ) {
+        openPopup();
+        
+      } else {
+      return console.log("game is still on");
+    }}
 
 
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+    if (playerScore === 5) {
+        document.getElementById('winnerName').innerHTML = "";
+        winnerName.textContent += "You win the game!!!";
+      //  return true;
+    } else if (computerScore === 5) {
+        document.getElementById('winnerName').innerHTML = "";
+        winnerName.textContent += "The computer wins the game!!!";
+       // return true;
+    } return false;
 }
 
-for (let i = 0; i < 5; i++) {
-    game();
+function replay(){
+    popup.classList.remove("open-popup");
+    playerScore = 0;
+    computerScore = 0;
 }
